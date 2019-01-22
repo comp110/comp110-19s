5a8286ca8662e05e5a5360a071edf57584c0a6ab
Kris Jordan <krisjordan@gmail.com>
2019-01-22 10:46:19 -0500

    Add lecture 3 materials

---
+++ src/03-conditionals-loops/8-ball-app.ts
@@ -0,0 +1,14 @@
+import { print, random, promptString } from "introcs";
+
+export let main = async () => {
+
+    let question = await promptString("Ask a Yes/No Question");
+    
+    let response: number;
+    // TODO #0 - Assign response a random number between 0 and 2
+    
+    // TODO #1 - Implement the response message logic 
+
+};
+
+main();
