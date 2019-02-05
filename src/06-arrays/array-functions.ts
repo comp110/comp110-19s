99c3e918af3a5715a3d33d246e7d6e58f912dbd5
Kris Jordan <krisjordan@gmail.com>
2019-02-05 11:42:14 -0500

    Add lec6 examples

---
+++ src/06-arrays/array-functions.ts
@@ -0,0 +1,20 @@
+/**
+ * Fill an array of length n with zeros.
+ */
+export let fillZeros = (n: number): number[] => {
+    return [];
+};
+
+/**
+ * Fill an array of numbers from low to high, inclusive.
+ */
+export let fillRange = (low: number, high: number): number[] => {
+    return [];
+};
+
+/**
+ * Given a number array, compute the sum of its elements.
+ */
+export let sum = (a: number[]): number => {
+    return 0;
+};
---
+++ src/06-arrays/array-practice-app.ts
@@ -0,0 +1,11 @@
+import { fillZeros, fillRange, sum } from "./array-functions";
+
+import { testArray, testNumber } from "./test-util";
+
+export let main = async () => {
+    
+    // TODO: Add Test Cases
+
+};
+
+main();
