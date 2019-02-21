fecd4ecb98fe3ca49861fc5ffb6cc624ae6f756e
Kris Jordan <krisjordan@gmail.com>
2019-02-21 10:45:06 -0500

    lec10 wip

---
+++ src/10-classes-and-objects/00-pizza-price-app.ts
@@ -0,0 +1,17 @@
+import { print } from "introcs";
+
+import { Pizza } from "./Pizza";
+
+import { price } from "./PizzaUtils";
+
+export let main = async () => {
+
+    // 1. Initialize a variable that holds a Pizza object and print it
+
+    // 2. Assign different values to each of its properties
+
+    // 3. Compute and print its price with the imported price function
+
+};
+
+main();
---
+++ src/10-classes-and-objects/01-pizza-order-app.ts
@@ -0,0 +1,49 @@
+import { print, promptString, promptNumber } from "introcs";
+import { Pizza } from "./Pizza";
+import { price } from "./PizzaUtils";
+
+export let main = async () => {
+
+    print("Welcome to Alfredo's Pizza!");
+    print("May I take your order?");
+
+    let order: Pizza[] = [];
+
+    let isOrdering = true;
+    while (isOrdering) {
+        // Construct Pizza
+        let pizza = new Pizza();
+        pizza.size = await promptString("What size? small, medium, large");
+        pizza.extraCheese = await promptString("Extra cheese? yes / no") === "yes";
+        pizza.toppings = await promptNumber("How many toppings?");
+        // Append Pizza to order
+        order[order.length] = pizza;
+        // Add another Pizza?
+        isOrdering = await promptString("Order another? yes / no") === "yes";
+    }
+
+    print("The order is...");
+    // TODO 1: Print the order
+
+    print("The first Pizza is...");
+    // TODO 2: Print the 1st pizza at index 0
+
+    print("The first Pizza's size is...");
+    // TODO 3: Print the 1st pizza's size
+
+    // TODO: Next Hands On Starts Here
+
+};
+
+let orderPrice = (pizzas: Pizza[]): number => {
+    let total = 0;
+
+    // TODO: Calculate the total price of an array of Pizzas
+    for (let i = 0; i < pizzas.length; i++) {
+        total += price(pizzas[i]);
+    }
+
+    return total;
+};
+
+main();
