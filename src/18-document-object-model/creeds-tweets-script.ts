f57a01bb606953aef829c5a0a3cc76c3b254bf3b
Kris Jordan <krisjordan@gmail.com>
2019-04-09 08:51:24 -0400

    Add lecture 18 assets

---
+++ src/18-document-object-model/creeds-tweets-script.ts
@@ -0,0 +1,11 @@
+// TODO: Uncomment these variables as needed.
+// let text = document.getElementById("text") as HTMLInputElement;
+// let characters = document.getElementById("characters") as HTMLSpanElement;
+// let form = document.getElementById("form") as HTMLFormElement;
+// let tweets = document.getElementById("tweets") as HTMLDivElement;
+
+export let main = async () => {
+    // TODO: Add Event Handlers
+};
+
+main();
